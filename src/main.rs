65aaf1403f96187c3daa569a4a63f48b32d8d283
dyatelok <gamovoleksii@gmail.com>
2024-01-07 20:24:44 +0100

    works!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use error_iter::ErrorIter as _;
 use log::error;
 use pixels::{Error, Pixels, SurfaceTexture};
+use rand::prelude::*;
 use std::time::Instant;
 use winit::dpi::LogicalSize;
 use winit::event::{Event, VirtualKeyCode};
@@ -8,8 +9,8 @@ use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::WindowBuilder;
 use winit_input_helper::WinitInputHelper;
 
-const WIDTH: u32 = 64;
-const HEIGHT: u32 = 32;
+const WIDTH: usize = 64;
+const HEIGHT: usize = 32;
 const TARGET_FPS: u64 = 60;
 const IPF: usize = 12; // instructions per frame
 
@@ -30,11 +31,11 @@ fn main() -> Result<(), Error> {
     let mut pixels = {
         let window_size = window.inner_size();
         let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(WIDTH, HEIGHT, surface_texture)?
+        Pixels::new(WIDTH as u32, HEIGHT as u32, surface_texture)?
     };
 
     let mut interpreter = Interpreter::new();
-    interpreter.load("IBM Logo.ch8").unwrap();
+    interpreter.load("roms/IBM Logo.ch8").unwrap();
 
     event_loop.run(move |event, _, control_flow| {
         let start_time = Instant::now();
@@ -92,7 +93,7 @@ fn log_error<E: std::error::Error + 'static>(method_name: &str, err: E) {
 
 struct Interpreter {
     memory: Vec<u8>,
-    screen: [bool; (WIDTH * HEIGHT) as usize],
+    screen: [[bool; WIDTH]; HEIGHT],
     program_counter: usize,
     index: u16,
     stack: Vec<u16>,
@@ -107,7 +108,7 @@ impl Interpreter {
     fn new() -> Self {
         Self {
             memory: vec![0; 4096],
-            screen: [false; (WIDTH * HEIGHT) as usize],
+            screen: [[false; WIDTH]; HEIGHT],
             program_counter: 0,
             index: 0,
             stack: vec![0; 16],
@@ -121,7 +122,8 @@ impl Interpreter {
 
     fn load(&mut self, filename: &str) -> Result<(), std::io::Error> {
         let bytes = std::fs::read(filename)?;
-        self.memory[0..bytes.len()].copy_from_slice(&bytes);
+        let offset = 0x200;
+        self.memory[offset..offset + bytes.len()].copy_from_slice(&bytes);
         Ok(())
     }
 
@@ -150,45 +152,190 @@ impl Interpreter {
         let nnn = opcode & 0x0FFF;
 
         match (c, x, y, n) {
-            (0x0, 0x0, 0x0, 0x0) => {
-                self.halt = true;
-                println!("program halt!");
-            }
             (0x0, 0x0, 0xE, 0x0) => {
-                for pix in &mut self.screen {
-                    *pix = false;
+                //Clear the screen
+                for row in &mut self.screen {
+                    for pix in row {
+                        *pix = false;
+                    }
                 }
             }
-            (0x1, _, _, _) => {
+            (0x0, 0x0, 0xE, 0xE) => {
+                //Return from a subroutine
+                let addr = self.stack.pop().unwrap();
+                self.program_counter = addr as usize;
+                todo!()
+            }
+            (0x0, ..) => {
+                //TODO Execute machine language subroutine at address NNN
+                // panic!()
+            }
+            (0x1, ..) => {
+                //Jump to address NNN
                 self.program_counter = nnn as usize;
             }
-            (0x6, _, _, _) => {
-                self.registers[x as usize] = nn as u8;
+            (0x2, ..) => {
+                // Execute subroutine starting at address NNN
+                self.program_counter = nnn as usize;
+                self.stack.push(nnn);
+                todo!()
             }
-            (0x7, _, _, _) => {
-                self.registers[x as usize] += nn as u8;
-            }
-            (0xA, _, _, _) => {
-                self.index = nnn;
-            }
-            (0xD, _, _, _) => {
-                for i in 0..n {
-                    self.display_byte(x, y + i, self.memory[self.program_counter]);
-                    self.program_counter += 1;
+            (0x3, ..) => {
+                //Skip the following instruction if the value of register VX equals NN
+                if self.registers[x as usize] == nn as u8 {
+                    self.program_counter += 2;
                 }
             }
-            _ => panic!(
-                "opcode {:04x} has not yet been implemented or just a wrong opcode",
-                opcode
-            ),
-        }
-    }
+            (0x4, ..) => {
+                //Skip the following instruction if the value of register VX is not equal to NN
+                if self.registers[x as usize] != nn as u8 {
+                    self.program_counter += 2;
+                }
+            }
+            (0x5, ..) => {
+                //Skip the following instruction if the value of register VX is equal to the value of register VY
+                if self.registers[x as usize] == self.registers[y as usize] {
+                    self.program_counter += 2;
+                }
+            }
+            (0x6, ..) => {
+                //Store number NN in register VX
+                self.registers[x as usize] = nn as u8;
+            }
+            (0x7, ..) => {
+                //Add the value NN to register VX
+                self.registers[x as usize] += nn as u8;
+            }
+            (0x8, _, _, 0x0) => {
+                //Store the value of register VY in register VX
+                self.registers[x as usize] = self.registers[y as usize];
+            }
+            (0x8, _, _, 0x1) => {
+                //Set VX to VX OR VY
+                self.registers[x as usize] |= self.registers[y as usize];
+            }
+            (0x8, _, _, 0x2) => {
+                //Set VX to VX AND VY
+                self.registers[x as usize] &= self.registers[y as usize];
+            }
+            (0x8, _, _, 0x3) => {
+                //Set VX to VX XOR VY
+                self.registers[x as usize] ^= self.registers[y as usize];
+            }
+            (0x8, _, _, 0x4) => {
+                //TODO Add the value of register VY to register VX
+                //Set VF to 01 if a carry occurs
+                //Set VF to 00 if a carry does not occur
+                self.registers[x as usize] += self.registers[y as usize];
+            }
+            (0x8, _, _, 0x5) => {
+                //TODO Subtract the value of register VY from register VX
+                //Set VF to 00 if a borrow occurs
+                //Set VF to 01 if a borrow does not occur
+                self.registers[x as usize] -= self.registers[y as usize];
+            }
+            (0x8, _, _, 0x6) => {
+                //TODO Store the value of register VY shifted right one bit in register VX¹
+                // Set register VF to the least significant bit prior to the shift
+                // VY is unchanged
+                self.registers[x as usize] = self.registers[y as usize] >> 1;
+            }
+            (0x8, _, _, 0x7) => {
+                //TODO Set register VX to the value of VY minus VX
+                //Set VF to 00 if a borrow occurs
+                //Set VF to 01 if a borrow does not occur
+                self.registers[x as usize] =
+                    self.registers[y as usize] - self.registers[x as usize];
+            }
+            (0x8, _, _, 0xE) => {
+                //Store the value of register VY shifted left one bit in register VX¹
+                //Set register VF to the most significant bit prior to the shift
+                //VY is unchanged
+                self.registers[x as usize] = self.registers[y as usize] << 1;
+            }
+            (0x9, ..) => {
+                //Skip the following instruction if the value of register VX is not equal to the value of register VY
+                if self.registers[x as usize] != self.registers[y as usize] {
+                    self.program_counter += 2;
+                }
+            }
+            (0xA, ..) => {
+                self.index = nnn;
+            }
+            (0xB, ..) => {
+                //Jump to address NNN + V0
+                self.program_counter = nnn as usize + self.registers[0] as usize;
+            }
+            (0xC, ..) => {
+                //Set VX to a random number with a mask of NN
+                self.registers[x as usize] = rand::random::<u8>() & nn as u8;
+            }
+            (0xD, ..) => {
+                //TODO Draw a sprite at position VX, VY with N bytes of sprite data starting at the address stored in I
+                // Set VF to 01 if any set pixels are changed to unset, and 00 otherwise
+                for i in 0..n as usize {
+                    let byte = self.memory[self.index as usize + i];
+                    let x = self.registers[x as usize] as usize % WIDTH;
+                    let y = self.registers[y as usize] as usize % HEIGHT;
 
-    fn display_byte(&mut self, x: u8, y: u8, byte: u8) {
-        let pos = (x as u32 + y as u32 * WIDTH) as usize;
-        let bytes: &mut [bool] = &mut self.screen[pos..pos + 8];
-        for (i, bit) in bytes.iter_mut().enumerate() {
-            *bit = (byte >> (7 - i)) % 2 == 1;
+                    let bits: &mut [bool] = &mut self.screen[y + i][x..x + 8];
+                    for (i, bit) in bits.iter_mut().enumerate() {
+                        *bit = (byte >> (7 - i)) % 2 == 1;
+                    }
+                }
+            }
+            (0xE, _, 0x9, 0xE) => {
+                //TODO Skip the following instruction if the key corresponding to the hex value currently stored in register VX is pressed
+                todo!()
+            }
+            (0xE, _, 0xA, 0x1) => {
+                //TODO Skip the following instruction if the key corresponding to the hex value currently stored in register VX is not pressed
+                todo!()
+            }
+            (0xF, _, 0x0, 0x7) => {
+                //TODO	Store the current value of the delay timer in register VX
+                todo!()
+            }
+            (0xF, _, 0x0, 0xA) => {
+                //TODO	Wait for a keypress and store the result in register VX
+                todo!()
+            }
+            (0xF, _, 0x1, 0x5) => {
+                //TODO	Set the delay timer to the value of register VX
+                todo!()
+            }
+            (0xF, _, 0x1, 0x8) => {
+                //TODO	Set the sound timer to the value of register VX
+                todo!()
+            }
+            (0xF, _, 0x1, 0xE) => {
+                //TODO	Add the value stored in register VX to register I
+                todo!()
+            }
+            (0xF, _, 0x2, 0x9) => {
+                //TODO	Set I to the memory address of the sprite data corresponding to the hexadecimal digit stored in register VX
+                todo!()
+            }
+            (0xF, _, 0x3, 0x3) => {
+                //TODO	Store the binary-coded decimal equivalent of the value stored in register VX at addresses I, I + 1, and I + 2
+                todo!()
+            }
+            (0xF, _, 0x5, 0x5) => {
+                //TODO	Store the values of registers V0 to VX inclusive in memory starting at address I
+                //I is set to I + X + 1 after operation²
+                todo!()
+            }
+            (0xF, _, 0x6, 0x5) => {
+                //TODO	Fill registers V0 to VX inclusive with the values stored in memory starting at address I
+                //I is set to I + X + 1 after operation²
+                todo!()
+            }
+            _ => {
+                panic!(
+                    "opcode {:04x} has not yet been implemented or just a wrong opcode",
+                    opcode
+                );
+            }
         }
     }
 
@@ -217,7 +364,7 @@ impl Interpreter {
 
     fn draw(&self, frame: &mut [u8]) {
         for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
-            let rgba = if self.screen[i] {
+            let rgba = if self.screen[i / WIDTH][i % WIDTH] {
                 [0x0, 0x0, 0x0, 0xff]
             } else {
                 [0xff, 0xff, 0xff, 0xff]
